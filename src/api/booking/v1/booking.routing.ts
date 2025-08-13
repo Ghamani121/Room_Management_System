8d41cd1929022a58d80075b13b1fcd803efd434c
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-08-13 11:13:12 +0530

    updated it in booking and room routes as well

--- src/api/booking/v1/booking.routing.ts
+++ src/api/booking/v1/booking.routing.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import * as bookingController from './booking.controller'
 import * as bookingValidation from './booking.validation';
+import {validateObjectId} from '../../../middlewares/validateobjectid';
 
 const router=Router();
 
 // router.post('/',  bookingValidation.createbookingValidation  ,bookingController.createbooking);
+
+
 // router.get('/',  bookingController.getbooking);
-// router.get('/:id',  bookingController.getbookingById);
-// router.put('/:id',  bookingValidation.updatebookingValidation,  bookingController.updatebookingById);
-// router.delete('/:id',  bookingValidation.deletebookingValidation,  bookingController.deletebookingById);
+// router.get('/:id', validateObjectId("id"),  bookingController.getbookingById);
+
+// //no id in the url sends bad request error
+// router.put('/', (req, res) => {res.status(400).json({ message: 'id is required' });});
+// router.put('/:id', validateObjectId("id"), bookingValidation.updatebookingValidation,  bookingController.updatebookingById);
+
+// router.delete('/', (req, res) => {res.status(400).json({ message: 'id is required' });});
+// router.delete('/:id', validateObjectId("id"), bookingValidation.deletebookingValidation,  bookingController.deletebookingById);
 
 export default router;
 
