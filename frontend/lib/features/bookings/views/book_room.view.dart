aec78f4995684ad579a6022a77b1d74d091a7b33
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-08-30 00:12:06 +0530

    updated

--- frontend/lib/features/bookings/views/book_room.view.dart
+++ frontend/lib/features/bookings/views/book_room.view.dart
@@ -130,6 +130,7 @@ class _BookRoomViewState extends State<BookRoomView> {
             errorText: viewModel.roomError, // <-- show error
           ),
           hint: const Text("Select Room"),
+          dropdownColor: Colors.white,
           items: const [
             DropdownMenuItem(value: "room1_id", child: Text("Bhishma")),
             DropdownMenuItem(value: "room2_id", child: Text("Ajeya")),
