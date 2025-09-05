3020ca3aeba985f298353b3abbd091d165714382
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-09-05 09:22:43 +0530

    i have applied provider

--- frontend/lib/config.service.dart
+++ frontend/lib/config.service.dart
@@ -1,4 +1,4 @@
 class ConfigService {
   // Hardcoded ngrok URL for testing
-  static String baseUrl = "https://9971226c3d1f.ngrok-free.app/api";
+  static String baseUrl = "https://3ff839f8439e.ngrok-free.app/api";
 }
---
+++ frontend/lib/features/auth/providers/auth.provider.dart
@@ -0,0 +1,30 @@
+// lib/features/auth/providers/auth_provider.dart
+import 'package:flutter/material.dart';
+import '../login.model.dart';
+import 'package:rms/features/auth/services/login.service.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final LoginService _service = LoginService();
+
+  Welcome? _authData;
+  Welcome? get authData => _authData;
+
+  bool get isLoggedIn => _authData != null;
+
+  Future<bool> login(String email, String password) async {
+    try {
+      final result = await _service.login(email, password);
+      _authData = result;
+      notifyListeners(); // tell the UI to rebuild
+      return true;
+    } catch (e) {
+      debugPrint("Auth login failed: $e");
+      return false;
+    }
+  }
+
+  void logout() {
+    _authData = null;
+    notifyListeners();
+  }
+}
