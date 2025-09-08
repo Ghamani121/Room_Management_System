dc6ceb423fce78903eeedfba98d47dbf7bdb72f2
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-09-08 10:04:50 +0530

    filtering is finlay working

--- frontend/lib/config.service.dart
+++ frontend/lib/config.service.dart
@@ -1,4 +1,4 @@
 class ConfigService {
   // Hardcoded ngrok URL for testing
-  static String baseUrl = "https://349ad670aada.ngrok-free.app/api";
+  static String baseUrl = "https://6b2703c9b523.ngrok-free.app/api";
 }
